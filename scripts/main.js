a841e93739c1f58128d8fc66b7cc5d9ee49948c4
DanLeon1 <danielleones402@gmail.com>
2023-02-17 23:23:30 -0500

    tarea

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,25 @@
 'use restrict';
 import { validateString, vAvg, checkAvg} from './validate.js';
-import {paintCard,addStudent, modalAlert, addProfesor} from './paint.js';
+import {paintCard,addStudent,addTeacher, modalAlert} from './paint.js';
 
 const btnAgregar = document.getElementById('btnAgregar');
 const btnMostrar = document.getElementById('btnMostrar');
+const cambiar = document.getElementById('opcion');
 
+cambiar.onchange = function (){
+    let op = document.getElementById('opcion').value; 
+    if (op === 'profesor'){
+        modalAlert("opcion profesor")
+        document.getElementById('text1').innerText="Nombres y Apellidos:";
+        document.getElementById('text2').innerText="Profesion:";
+        document.getElementById('text3').innerText="Edad:";
+    } else if (op === 'estudiante'){
+        modalAlert("opcion estudiante")
+        document.getElementById('text1').innerText="Nombres:";
+        document.getElementById('text2').innerText="Apellidos:";
+        document.getElementById('text3').innerText="Promedio:";
+    }
+}
 /*generar eventos 
     1. Colocando el metodo (evento) en el atributo onclick,
     onmouseover, onmouseout de la etiqueta
@@ -16,25 +31,51 @@ btnAgregar.onclick = function(){
     const name = document.getElementById('nombre').value;
     const lastName = document.getElementById('apellido').value;
     const avg = parseFloat(document.getElementById('promedio').value);
+    const nomYap= document.getElementById('nombre').value;
+    const frof= document.getElementById('apellido').value;
+    const años=parseFloat( document.getElementById('promedio').value);
     const op = document.getElementById('opcion').value;
+    
 
     //console.log<(`${name} ${lastname} ${avg} ${op}`);
+    if(op==='estudiante'){
+        if (validateString(name) && validateString (lastName) && op != 0){
+            if ((!isNaN(avg)) && (vAvg(avg))){
+                addStudent(name,lastName,avg)
+                modalAlert('se agrego estudiante')
+                document.getElementById('nombre').value="";
+                document.getElementById('apellido').value="";
+                document.querySelector('#promedio').value="";
+            }else{
+                document.querySelector('#promedio').value="";
+                modalAlert("promedio inválido");
+            }
 
-    if (validateString(name) && validateString (lastName) && op != 0){
-        if ((!isNaN(avg)) && (vAvg(avg))){
-            addStudent(name,lastName,avg)
         }else{
-            document.querySelector('#promedio').value="";
-            modalAlert("promedio inválido");
+            modalAlert("datos invalidos, revisar los datos");
         }
-
-    }else{
-        modalAlert("datos invalidos, revisar los datos");
-    }
-    formulario.reset()
+    }else if(op==='profesor'){
+        if(validateString(nomYap)&& validateString(frof) && op != 0){
+            if(!isNaN(años)){
+                addTeacher(name,lastName,avg)
+                modalAlert('se agrego profesor')
+                document.getElementById('nombre').value="";
+                document.getElementById('apellido').value="";
+                document.querySelector('#promedio').value="";
+            }
+        }else{
+            modalAlert("datos invalidos, revisar los datos");
+        }
+    }    
+       
 }
-btnMostrar.addEventListener("click", function () {
-    paintCard("ESTUDIANTE");    
+btnMostrar.addEventListener('click', function () {
+       const op = document.getElementById('opcion').value;
+       if(op ==='estudiante'){
+        paintCard('ESTUDIANTE');
+       }else if (op ==='profesor'){
+        paintCard('PROFESOR');
+       }
 })
 
 //primera tarea agregar el modal
--- scripts/paint.js
+++ scripts/paint.js
@@ -4,7 +4,7 @@ import {checkAvg } from './validate.js';
 const cardE = document.getElementById('cardsEstudiantes');
 const students = [];
 const cardP = document.getElementById('cardseProfesores');
-const profesor = [];
+const profes =[];
 
 const paintCard = (tipo) =>{
     console.log(tipo);
@@ -12,29 +12,27 @@ const paintCard = (tipo) =>{
     const fragment = document.createDocumentFragment();
     const templateStudent = document.querySelector('#templateEstudiante').content;
     const templateProfesor = document.querySelector('#templateProfesor').content;
+
     if (tipo=== 'ESTUDIANTE'){
        for(let i of students){
         const cloneTemp = templateStudent.cloneNode(true);
         cloneTemp.querySelector('.title-card').innerHTML= "datos del estudiante";
-        cloneTemp.querySelector('.data-card').innerHTML = `nombre: ${i.nom.toUpperCase()} apellidos: ${i.ape.toUpperCase()}`;
-        cloneTemp.querySelector('.text-promedio').innerHTML = `promedio es: ${i.prom}`;
+        cloneTemp.querySelector('.data-card').innerHTML = `NOMBRE: ${i.nom.toUpperCase()} APELLIDOS: ${i.ape.toUpperCase()}`;
+        cloneTemp.querySelector('.text-promedio').innerHTML = `PROMEDIO ES : ${i.prom}`;
         cloneTemp.querySelector('.text-aprobado').innerHTML = `${checkAvg(i.prom)}`;
         fragment.appendChild(cloneTemp)
        } 
-    }else{
-        //pintar a profesor
-        if(tipo==='PROFESOR'){
-            for(let i of profesor ){
-                const cloneTemp = templateProfesor.cloneNode(true);
-                cloneTemp.querySelector('.titleCard').innerHTML= "datos del profesor ";
-                cloneTemp.querySelector('.nombreApellido').innerHTML = `nombre: ${i.nom.toUpperCase()} apellidos: ${i.ape.toUpperCase()}`;
-                cloneTemp.querySelector('.profecion').innerHTML = `profesion es: ${i.prom}`;
-                
-            }
-
-
+    }else if (tipo==='PROFESOR'){
+        for(let i of profes){
+            const cloneTemp = templateProfesor.cloneNode(true);
+            cloneTemp.querySelector('.title-card').innerHTML= "datos del profesor";
+            cloneTemp.querySelector('.data-card').innerHTML = `MOMBRE Y APELLIDO: ${i.nomYap.toUpperCase()} PROFESION ${i.frof.toUpperCase()}`;
+            cloneTemp.querySelector('.text-promedio').innerHTML = `LOS AÑOS SON: ${i.años}`;
+            fragment.appendChild(cloneTemp)
         }
     }
+        
+    
     cardE.appendChild(fragment);
     cardP.appendChild(fragment);
 }
@@ -46,17 +44,17 @@ const addStudent = (name,lastName, avg)=>{
         prom : avg
     }
     students.push(student);
-    modalAlert('se agrego estudiante');
 };
-
-const addProfesor =(nombreApellido, profecion ) => {
-    let profesor ={
-        nomapp: nombreApellido,
-        prof: profecion
+const addTeacher = (name,lastname,avg)=>{
+    let teacher={
+        nomYap: name,
+        frof: lastname,
+        años: avg
     }
-    profesor.push(profesor);
-    modalAlert('se agrego profesor');
-}
+    profes.push(teacher);
+   
+
+};
 
 const modalAlert = (cad) => {
     const alerta =document.createElement('div');
@@ -78,4 +76,4 @@ const modalAlert = (cad) => {
 
 }
 
-export{paintCard,addStudent,addProfesor, modalAlert }
+export{paintCard,addStudent,addTeacher, modalAlert }
